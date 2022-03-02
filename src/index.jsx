ff6c775fe66ca7ddd57dcd1c2be436d97495bf49
jeonghun10 <jeonghun8910@gmail.com>
2022-03-02 13:27:32 +0900

    Feat : Suspense 부분 로딩 컴포넌트 추가

--- src/index.jsx
+++ src/index.jsx
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { RecoilRoot } from 'recoil';
 import { BrowserRouter } from 'react-router-dom';
+import Loading from './components/Loading'
 
 ReactDOM.render(
   <React.StrictMode>
     <RecoilRoot>
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<Loading/>}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
